993213a1ddb1899930566e122c32463877bdba6c
Sophal Kim <sophal.kim.work@gmail.com>
2014-06-26 16:30:45 -0700

    I like parenthesis for organizational purposes

---
+++ CodingBat_Practice4/src/Or35.java
@@ -0,0 +1,13 @@
+
+public class Or35 {
+
+	public static boolean or35(int n) {
+		return (n % 3 == 0) || (n % 5 == 0);	
+	}
+	
+	
+	public static void main(String[] args) {
+		int n = 3;
+		System.out.println(or35(n));
+	}
+}
