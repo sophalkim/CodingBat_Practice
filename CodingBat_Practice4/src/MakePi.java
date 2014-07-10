bc15af68fcd06412e00789afa8f0bb331226b024
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-09 20:49:18 -0700

    Simple array creation

---
+++ CodingBat_Practice4/src/MakePi.java
@@ -0,0 +1,13 @@
+
+public class MakePi {
+
+	public static int[] makePi() {
+		int[] x = {3, 1 ,4};
+		return x;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(makePi());
+	}
+}
