04bac7db39402a43389737d4b71a33a97d5c23db
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-11 13:15:53 -0700

    Not sure why this equation isn't working

---
+++ CodingBat_Practice4/src/WithoutX.java
@@ -0,0 +1,24 @@
+
+public class WithoutX {
+
+	public static String withoutX(String str) {
+		int len = str.length();
+		String x = "";
+		for (int i = 0; i < len; i++) {
+			if (str.substring(i, i + 1).equals("x")) {
+				if (i != 0 || i != len) {
+					x += str.substring(i, i + 1);
+				}
+			} else {
+				x+= str.substring(i, i + 1);
+			}
+		}
+		return x;
+	}
+	
+	
+	public static void main(String[] args) {
+		String str = "Hxix";
+		System.out.println(withoutX(str));
+	}
+}
