ec291162892c8206cdef4a870b10e6daa94934d7
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-20 00:34:39 -0700

    Very Tough, my solution isn't working

---
+++ CodingBat_Practice4/src/GetSandwich.java
@@ -0,0 +1,38 @@
+
+public class GetSandwich {
+
+	public static String getSandwich(String str) {
+		int len = str.length();
+		String b = "bread";
+		int count = 0;
+		for (int i = 0; i < len - 4; i++) {
+			if (str.substring(i, i + 5).equals(b)) {
+				count++;
+			}
+		}
+		if (count <= 1) {
+			return "";
+		}
+		int start = 0;
+		for (int i = 0; i < len - 5; i++) {
+			if (str.substring(i, i + 5).equals(b)) {
+				start = i + 5;
+				break;
+			}
+		}
+		int end = len;
+		for (int i = len; i >= 5; i--) {
+			if (str.substring(i - 5, i).equals(b)) {
+				end = len - i;
+				break;
+			}
+		}
+		return str.substring(start, end);	
+	}
+	
+	
+	public static void main(String[] args) {
+		String str = "xxbreadjambreadyy";
+		System.out.println(getSandwich(str));
+	}
+}
