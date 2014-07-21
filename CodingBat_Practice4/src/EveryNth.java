27f0fb3fcd983d42e0efaea1007d9c630375147f
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-21 15:51:55 -0700

    Toughest problem on String 1

---
+++ CodingBat_Practice4/src/EveryNth.java
@@ -0,0 +1,21 @@
+
+public class EveryNth {
+
+	public static String EveryNth(String str, int n) {
+		int len = str.length();
+		String x = "";
+		for (int i = 0; i < len; i += n) {
+			if (i + n < len) {
+				x += str.charAt(i);
+			}
+		}
+		return x;
+	}
+	
+	
+	public static void main(String[] args) {
+		String str = "Miracle";
+		int n = 2;
+		System.out.println(EveryNth(str, n));
+	}
+}
