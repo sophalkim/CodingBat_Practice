9d69cf29d705eaaf722ff3b41b21dafd56815700
Sophal Kim <sophal.kim.work@gmail.com>
2014-08-26 08:36:33 -0700

    Simple "Or" check

---
+++ CodingBat_Practice4/src/Lucky13.java
@@ -0,0 +1,18 @@
+
+public class Lucky13 {
+
+	public static boolean lucky13(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1 || nums[i] == 3) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3};
+		System.out.println(lucky13(nums));
+	}
+}
