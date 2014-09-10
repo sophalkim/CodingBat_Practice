821a9e5b096609200dd67ff4ee0a418fca100a28
Sophal Kim <sophal.kim.work@gmail.com>
2014-09-10 09:12:59 -0700

    Added length test, to avoid index out of bounds

--- CodingBat_Practice4/src/AltPairs.java
+++ CodingBat_Practice4/src/AltPairs.java
@@ -1,8 +1,10 @@
 public String altPairs(String str) {
 	String x = "";
-	for (int i = 0; i < str.length; i+=4) {
+	for (int i = 0; i < str.length(); i+=4) {
 		x += str.charAt(i);
-		x += str.charAt(i + 1);
+		if (i + 1 < str.length()) {
+			x += str.charAt(i + 1);
+		}
 	}
 	return x;
 }
