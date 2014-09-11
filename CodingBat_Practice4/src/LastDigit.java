af9498ee0b89a2a22f535eee781a4b2c887a872c
Sophal Kim <sophal.kim.work@gmail.com>
2014-09-11 08:26:24 -0700

    Find remainder, check if they equal

--- CodingBat_Practice4/src/LastDigit.java
+++ CodingBat_Practice4/src/LastDigit.java
@@ -1,19 +1,6 @@
-
-public class LastDigit {
-
-	public static boolean lastDigit(int a, int b) {
-		int x = a % 10;
-		int y = b % 10;
-		if (x == y) {
-			return true;
-		}
-		return false;	
-	}
-	
-	
-	public static void main(String[] args) {
-		int a = 7;
-		int b = 17;
-		System.out.println(lastDigit(a, b));
-	}
+public boolean lastDigit(int a, int b, int c) {
+	int x = a % 10;
+	int y = b % 10;
+	int z = c % 10;
+	return (x == y || x == z || y == z);
 }
