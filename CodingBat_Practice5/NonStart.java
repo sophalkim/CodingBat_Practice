6363a8b0d8ac5d49a5ef0610066581604fb37959
sophalkim <sophal.kim.work@gmail.com>
2014-09-18 10:05:40 -0700

    Check for length 1 or less to return the other string

---
+++ CodingBat_Practice5/NonStart.java
@@ -0,0 +1,11 @@
+public String nonStart(String a, String b) {
+	int lenA = a.length();
+	int lenB = b.length();
+	if (lenA <= 1) {
+		return b.substring(1, lenB);
+	}
+	if (lenB <= 1) {
+		return a.substring(1, lenA);
+	}
+	return a.substring(1, a.length()) + b.substring(1, b.length());
+}
