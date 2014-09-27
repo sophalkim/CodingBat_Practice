7c314f5ee0aadcf4c2c43f2a62cf9191f4485c30
Billy Bob <Billy Bob@billybob.gateway.2wire.net>
2014-09-27 11:03:24 -0700

    Almost solved it, just one more case

--- CodingBat_Practice5/StarOut.java
+++ CodingBat_Practice5/StarOut.java
@@ -1,7 +1,14 @@
 public String startOut(String str) {
 	String x = "";
 	int len = str.length();
-	for (int i = 0; i < len; i++) {
+	int count = 0;
+	if (len <= 0) {
+		return x;
+	}
+	if (str.charAt(0) == '*') {
+		count = 2;
+	}
+	for (int i = count; i < len; i++) {
 		if (i + 1 < len && str.charAt(i + 1) == '*') {
 			if (i+ 2 < len && str.charAt(i + 2) == '*') {
 				i+=3;
