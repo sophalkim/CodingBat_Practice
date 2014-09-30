b045208171014adcbbce82775e3c548b45466944
Billy Bob <Billy Bob@billybob.gateway.2wire.net>
2014-09-30 09:37:49 -0700

    Had to use local booleans to solve

---
+++ CodingBat_Practice5/No14.java
@@ -0,0 +1,12 @@
+public boolean no14(int[] nums) {
+	boolean one = false;
+	boolean four = false;
+	for (int i = 0; i < nums.length; i++) {
+		if (nums[i] == 1) one = true;
+		if (nums[i] == 4) four = true;
+	}
+	if (one == true && four == true) {
+		return false;
+	}
+	return true;
+}
