d344d0cf4587cc5f2f6f25b4b9342a1567ec98da
sophalkim <sophal.kim.work@gmail.com>
2014-10-08 08:51:01 -0700

    omg, so many exception in order to finish this problem, not an elegant solution, but functional

--- CodingBat_Practice5/TwoTwo.java
+++ CodingBat_Practice5/TwoTwo.java
@@ -1,5 +1,11 @@
 public boolean twoTwo(int[] nums) {
 	int len = nums.length;
+	if (len <= 0) {
+		return true;
+	}
+	if (len <= 1 && nums[i] == 2) {
+		return false;
+	}
 	for (int i = 0; i < len; i++) {
 		if (nums[i] == 2) {
 			if (i == len - 1 && nums[i - 1] != 2) {
