12f7f5d3ae4636ece8dab65b18b137ce6fe3e9bf
sophalkim <sophal.kim.work@gmail.com>
2014-11-16 07:41:11 -0800

    Wrote helper method to practice decompostiion

---
+++ CodingBat_Practice5/NoTeenSum.java
@@ -0,0 +1,13 @@
+public int noTeenSum(int a, int b, int c) {
+	return fixTeen(a) + fixTeen(b) + fixTeen(c);
+}
+
+public int fixTeen(int n) {
+	if (n == 15 || n == 16) {
+		return n;
+	}
+	if (n >= 13 && n <= 19) {
+		return 0;
+	}
+	return n;
+}
