97e95667cf54dd7a9b94ce35965eda6bd5d50406
sophalkim <sophal.kim.work@gmail.com>
2014-11-20 09:37:10 -0800

    Create 3 locals differences with Math.abs().

--- CodingBat_Practice5/CloseFar.java
+++ CodingBat_Practice5/CloseFar.java
@@ -1,15 +1,11 @@
 public boolean closeFar(int a, int b, int c) {
 	int x = Math.abs(a - b);
-	int y = Math.abs(b - c);
-	int z = Math.abs(a - c);
-
-	if (x <= 1 && y >=2 || x <=1 && z >= 2) {
+	int y = Math.abs(a - c);
+	int z = Math.abs(b - c);
+	if (x <= 1 && y >= 2 && z >= 2) {
 		return true;
 	}
-	if (y <= 1 && x >= 2 || y <= 1 && z >= 2) {
-		return true;
-	}
-	if (z <= 1 && x >= 2 || z <= 1 && y >= 2) {
+	if (y <= 1 && x >= 2 && z >= 2) {
 		return true;
 	}
 	return false;
