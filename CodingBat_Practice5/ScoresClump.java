8d6b5d88ca8251d6c2231df7103b837586777dd2
sophalkim <sophal.kim.work@gmail.com>
2014-12-03 14:29:01 -0800

    Exanime 3 elements at a time
    Make sure their differences is equal or lower than 2

--- CodingBat_Practice5/ScoresClump.java
+++ CodingBat_Practice5/ScoresClump.java
@@ -1,12 +1,10 @@
 public boolean scoresClump(int[] scores) {
-	for (int i = 1; i < scores.length - 1; i++) {
-		if (Math.abs(scores[i - 1] - scores[i]) <= 2) {
-			if (Math.abs(scores[i] - scores[i + 1]) <= 2) {
-				if (Math.abs(scores[i - 1] - scores[i + 1]) <= 2) {
-					return true;
-				}
-			}
-		}
-	}
-	return false;
+    for (int i = 0; i < scores.length - 2; i++) {
+        if (scores[i + 2] - scores[i + 1] <= 2 && 
+            scores[i + 1] - scores[i] <= 2 &&
+            scores[i + 2] - scores[i] <= 2) {
+                return true;
+        }
+    }
+    return false;
 }
