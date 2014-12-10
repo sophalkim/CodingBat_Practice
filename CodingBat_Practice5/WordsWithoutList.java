208ec0d5f0b32c475e25e45312a10923bb74ce1e
sophalkim <sophal.kim.work@gmail.com>
2014-12-09 22:38:30 -0800

    Create List object, then check len to add to ArrayList

---
+++ CodingBat_Practice5/WordsWithoutList.java
@@ -0,0 +1,9 @@
+public List wordsWithoutList(String[] words, int len) {
+	ArrayList<String> list = new ArrayList<String>();
+	for (int i = 0; i < words.length; i++) {
+		if (words[i].length() != len) {
+			list.add(words[i]);
+		}
+	}
+	return list;
+}
