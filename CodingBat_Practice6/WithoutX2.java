46274804854f7b5b3708c93a3a94f4a8b8453cf2
sophalkim <sophal.kim.work@gmail.com>
2015-02-03 06:24:16 -0800

    The ordering is important without loops
    Check if both cases is true first, then the individual case

---
+++ CodingBat_Practice6/WithoutX2.java
@@ -0,0 +1,16 @@
+public String withoutX2(String str) {
+	int len = str.length();
+	
+	if (len >=2 && str.substring(0, 1).equals("x") && str.substring(1, 2).equals("x")) {
+		return str.substring(2);
+	}
+
+	if (len >=2 && str.substring(1, 2).equals("x")) {
+		return str.substring(0, 1) + str.substring(2);
+	}
+
+	if (len >= 1 && str.substring(0, 1).equals("x")) {
+		return str.substring(1);
+	}
+	return str;
+}
